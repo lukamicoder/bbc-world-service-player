7aef7a31512a4514734602e84a34520ef51e6f09
lukamicoder <lukamicoder@gmail.com>
2015-10-06 21:31:08 -0700

    Reorganized code; added more connection error checking

--- bbcworldserviceplayer/src/main/java/com/lukamicoder/bbcworldserviceplayer/MainActivity.java
+++ bbcworldserviceplayer/src/main/java/com/lukamicoder/bbcworldserviceplayer/MainActivity.java
@@ -15,7 +15,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.NotificationCompat;
-import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -28,12 +27,16 @@ public class MainActivity extends AppCompatActivity {
     private MediaPlayer mediaPlayer;
     private TextView textViewInfo;
     private Handler durationHandler = new Handler();
-    private Handler progressHandler = new Handler();
+    private Handler initHandler = new Handler();
+    private Handler noInternetHandler = new Handler();
     private Menu menu;
     private NotificationManager nmanager;
     private NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
-    private Integer dotCount = 0;
-    private Boolean isEnabled = false;
+    private Integer dotCount = -4;
+    private Integer noInternetInterval = 5000;
+    private Integer initProgressInterval = 500;
+    private Integer updateDurationInterval = 500;
+    private final Integer notificationID = 1;
     private final String ExitIntentString = "ExitIntent";
     private final static String PlayControlIntentString = "PlayControlIntent";
 
@@ -48,43 +51,59 @@ public class MainActivity extends AppCompatActivity {
         mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mp) {
-                isEnabled = true;
-                MenuInflater inflater = getMenuInflater();
-                inflater.inflate(R.menu.main_menu, menu);
-                textViewInfo.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
-                textViewInfo.setText(getString(R.string.start_pos));
-                progressHandler.removeCallbacks(updateProgress);
+                init();
             }
         });
 
         if (!isInternetAvailable()) {
             textViewInfo.setText(R.string.no_internet_msg);
+            noInternetHandler.postDelayed(checkInternet, noInternetInterval);
         } else {
-            try {
-                mediaPlayer.setDataSource(getString(R.string.url));
-                mediaPlayer.prepareAsync();
-                textViewInfo.setText(R.string.init);
-                progressHandler.postDelayed(updateProgress, 500);
-            } catch (IllegalArgumentException | SecurityException | IllegalStateException | IOException e) {
-                textViewInfo.setText(R.string.error_msg);
-            }
+            preparePlayer();
         }
 
         nmanager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         createNotification();
+    }
+
+    private void preparePlayer() {
+        noInternetHandler.removeCallbacks(checkInternet);
+
+        try {
+            mediaPlayer.setDataSource(getString(R.string.url));
+            mediaPlayer.prepareAsync();
+            textViewInfo.setText(R.string.init);
+            initHandler.postDelayed(updateInitProgress, initProgressInterval);
+        } catch (IllegalArgumentException | SecurityException | IllegalStateException | IOException e) {
+            textViewInfo.setText(R.string.error_msg);
+        }
+    }
+
+    private void init() {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        textViewInfo.setText(getString(R.string.start_pos));
+
+        Intent playControlIntent = new Intent(PlayControlIntentString);
+        PendingIntent actionPendingIntent = PendingIntent.getBroadcast(this, 0, playControlIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Intent exitIntent = new Intent(ExitIntentString);
+        PendingIntent exitPendingIntent = PendingIntent.getBroadcast(this, 0, exitIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        mBuilder.addAction(android.R.drawable.ic_media_play, getString(R.string.play), actionPendingIntent);
+        mBuilder.addAction(android.R.drawable.ic_menu_close_clear_cancel, getString(R.string.exit), exitPendingIntent);
+        nmanager.notify(notificationID, mBuilder.build());
 
         registerReceiver(exitIntentReceiver, new IntentFilter(ExitIntentString));
         registerReceiver(playControlIntentReceiver, new IntentFilter(PlayControlIntentString));
+
+        initHandler.removeCallbacks(updateInitProgress);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         this.menu = menu;
 
-        if (isEnabled) {
-            MenuInflater inflater = getMenuInflater();
-            inflater.inflate(R.menu.main_menu, menu);
-        }
         return true;
     }
 
@@ -110,24 +129,15 @@ public class MainActivity extends AppCompatActivity {
         mainIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
         PendingIntent mainPendingIntent = PendingIntent.getActivity(this, 0, mainIntent, 0);
 
-        Intent playControlIntent = new Intent(PlayControlIntentString);
-        PendingIntent actionPendingIntent = PendingIntent.getBroadcast(this, 0, playControlIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        Intent exitIntent = new Intent(ExitIntentString);
-        PendingIntent exitPendingIntent = PendingIntent.getBroadcast(this, 0, exitIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
         boolean isLollipop = (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP);
-
         Notification notification = mBuilder
                 .setContentTitle(getString(R.string.app_short_name))
                 .setContentText(getString(R.string.start_pos))
                 .setOngoing(true)
                 .setSmallIcon(isLollipop ? R.mipmap.ic_launcher_white : R.mipmap.ic_launcher)
-                .setContentIntent(mainPendingIntent)
-                .addAction(android.R.drawable.ic_media_play, getString(R.string.play), actionPendingIntent)
-                .addAction(android.R.drawable.ic_menu_close_clear_cancel, getString(R.string.exit), exitPendingIntent).build();
+                .setContentIntent(mainPendingIntent).build();
 
-        nmanager.notify(1, notification);
+        nmanager.notify(notificationID, notification);
     }
 
     private final BroadcastReceiver exitIntentReceiver = new BroadcastReceiver() {
@@ -147,9 +157,14 @@ public class MainActivity extends AppCompatActivity {
     private void unload() {
         mediaPlayer.stop();
         durationHandler.removeCallbacks(updateDuration);
-        progressHandler.removeCallbacks(updateProgress);
-        unregisterReceiver(exitIntentReceiver);
-        unregisterReceiver(playControlIntentReceiver);
+        initHandler.removeCallbacks(updateInitProgress);
+        noInternetHandler.removeCallbacks(checkInternet);
+        try {
+            unregisterReceiver(exitIntentReceiver);
+            unregisterReceiver(playControlIntentReceiver);
+        } catch (IllegalArgumentException ignored) {
+        }
+
         nmanager.cancelAll();
         finish();
     }
@@ -166,14 +181,14 @@ public class MainActivity extends AppCompatActivity {
         } else {
             menuItemPlayControl.setIcon(android.R.drawable.ic_media_pause);
             menuItemPlayControl.setTitle(getString(R.string.pause));
-            durationHandler.postDelayed(updateDuration, 500);
+            durationHandler.postDelayed(updateDuration, updateDurationInterval);
             mediaPlayer.start();
             isRunning = true;
         }
 
         mBuilder.mActions.get(0).icon = isRunning ? android.R.drawable.ic_media_pause : android.R.drawable.ic_media_play;
         mBuilder.mActions.get(0).title = isRunning ? getString(R.string.pause) : getString(R.string.play);
-        nmanager.notify(1, mBuilder.build());
+        nmanager.notify(notificationID, mBuilder.build());
     }
 
     private Runnable updateDuration = new Runnable() {
@@ -183,21 +198,37 @@ public class MainActivity extends AppCompatActivity {
                     TimeUnit.MILLISECONDS.toSeconds((long) timeElapsed) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long) timeElapsed)));
             textViewInfo.setText(duration);
 
-            nmanager.notify(1, mBuilder.setContentText(duration).build());
+            nmanager.notify(notificationID, mBuilder.setContentText(duration).build());
 
-            durationHandler.postDelayed(this, 500);
+            durationHandler.postDelayed(this, updateDurationInterval);
         }
     };
 
-    private Runnable updateProgress = new Runnable() {
+    private Runnable updateInitProgress = new Runnable() {
         public void run() {
-            if (dotCount++ < 3) {
-                textViewInfo.setText(textViewInfo.getText() + ".");
-            } else {
-                dotCount = 0;
-                textViewInfo.setText(R.string.init);
+            String text = getText(R.string.init).toString();
+
+            if (dotCount++ == 0) {
+                dotCount = -3;
             }
-            progressHandler.postDelayed(this, 500);
+
+            String padding = String.format("%" + (Math.abs(dotCount) + 1) + "s", " ");
+            text = text.substring(0, text.length() + dotCount) + padding;
+
+            textViewInfo.setText(text);
+
+            initHandler.postDelayed(this, initProgressInterval);
+        }
+    };
+
+    private Runnable checkInternet = new Runnable() {
+        public void run() {
+            if (isInternetAvailable()) {
+                preparePlayer();
+                return;
+            }
+
+            noInternetHandler.postDelayed(this, noInternetInterval);
         }
     };
 
@@ -206,6 +237,5 @@ public class MainActivity extends AppCompatActivity {
 
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
         return null != activeNetwork;
-
     }
 }
